35b88e8d9231442bbb7b6e6127cc7917590132b2
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 04:24:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 2 
     puts  array.join(" and ")
   elsif array.size > 2 
-    temp = array.slice(0, array.size - 1).join(",")
+    temp = array.slice(0, array.size - 1).join(", ")
     array = temp + " and " + array[-1]
   end
   puts array
