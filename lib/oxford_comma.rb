3989a963e8247395319d5269f1e320b70f56a236
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 04:30:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
   if array.size == 2 
+    return  array[0]
+  elsif array.size == 2 
     return  array.join(" and ")
   elsif array.size > 2 
     temp = array.slice(0, array.size - 1).join(", ")
